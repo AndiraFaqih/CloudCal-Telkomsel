4fb2437155d909669f153a41542b3ddfeb4eb631
AndiraFaqih <afaqihm@gmail.com>
2023-10-15 18:40:25 +0700

    style: tidy up the code

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.status(200).send({
         msg: "Welcome to Telkomsel Project API",
     });
 });
+app.use(require("./routes/indexRoute"));
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
 });
-app.use(require("./routes/indexRoute"));
 
 module.exports = app;
